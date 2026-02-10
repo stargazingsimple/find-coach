62dcbf521e058f15cc5c8d6152296ae33ab55cdd
stargazingsimple <stargazingsimple@gmail.com>
2026-02-10 16:44:40 +0200

    add store configuration and coach-item component

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  name: "CoachItem",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    areas: {
+      type: Array,
+      required: true,
+    },
+    hourlyRate: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li>
+    <h3>{{ firstName }} {{ lastName }}</h3>
+    <h4>$ {{ hourlyRate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link :to="`/coaches/${id}/contact`">Contact</router-link>
+      <router-link :to="`/coaches/${id}`">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/pages/CoachesList.vue
+++ src/pages/CoachesList.vue
@@ -1,6 +1,14 @@
 <script>
+import { mapGetters, mapState } from "vuex";
+import CoachItem from "@/components/coaches/CoachItem.vue";
+
 export default {
   name: "CoachesList",
+  components: { CoachItem },
+  computed: {
+    ...mapState("coaches", ["coaches"]),
+    ...mapGetters("coaches", ["hasCoaches"]),
+  },
 };
 </script>
 
@@ -11,8 +19,30 @@ export default {
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      COACHES LIST
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="{ id, firstName, lastName, areas, hourlyRate } in coaches"
+        :key="id"
+        :id="id"
+        :first-name="firstName"
+        :last-name="lastName"
+        :areas="areas"
+        :hourly-rate="hourlyRate"
+      />
     </ul>
+    <h3 v-else>Empty data</h3>
   </section>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
