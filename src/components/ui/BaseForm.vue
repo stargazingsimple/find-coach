257417db6a6e5b53befc9bb326511c354a0b2fec
stargazingsimple <stargazingsimple@gmail.com>
2026-02-13 20:08:02 +0200

    add base-form component

---
+++ src/components/ui/BaseForm.vue
@@ -0,0 +1,148 @@
+<script>
+import BaseButton from "@/components/ui/BaseButton.vue";
+
+export default {
+  name: "BaseForm",
+  components: { BaseButton },
+  props: {
+    fields: {
+      type: Array,
+      required: true,
+    },
+    submitButtonText: {
+      type: String,
+      default: "Submit",
+    },
+  },
+  data() {
+    return {
+      formData: {},
+    };
+  },
+  created() {
+    this.fields.forEach((field) => {
+      switch (field.type) {
+        case "options":
+          this.formData[field.id] = [];
+          break;
+        default:
+          this.formData[field.id] = null;
+      }
+    });
+  },
+  methods: {
+    submitForm() {
+      console.log(this.formData);
+      this.resetForm();
+    },
+    resetForm() {
+      this.formData = {};
+    },
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <template v-for="field in fields">
+      <div
+        v-if="field.type === 'textarea'"
+        class="form-control"
+        :key="field.id"
+      >
+        <label :for="field.id">{{ field.label }}</label>
+        <textarea rows="5" :id="field.id" v-model.trim="formData[field.id]" />
+      </div>
+      <div
+        v-else-if="field.type === 'number'"
+        class="form-control"
+        :key="field.id"
+      >
+        <label :for="field.id">{{ field.label }}</label>
+        <input
+          type="number"
+          :id="field.id"
+          v-model.number="formData[field.id]"
+        />
+      </div>
+      <div
+        v-else-if="field.type === 'options'"
+        class="form-control"
+        :key="field.id"
+      >
+        <h3>{{ field.label }}</h3>
+        <div v-for="{ id, label, value } in field.options" :key="id">
+          <input
+            type="checkbox"
+            :id="id"
+            :value="value"
+            v-model="formData[field.id]"
+          />
+          <label :for="id">{{ label }}</label>
+        </div>
+      </div>
+      <div v-else class="form-control" :key="field.id">
+        <label :for="field.id">{{ field.label }}</label>
+        <input type="text" :id="field.id" v-model.trim="formData[field.id]" />
+      </div>
+    </template>
+    <base-button>{{ submitButtonText }}</base-button>
+  </form>
+</template>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type="checkbox"] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type="checkbox"] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type="checkbox"]:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+</style>
--- src/pages/CoachRegistration.vue
+++ src/pages/CoachRegistration.vue
@@ -1,9 +1,50 @@
 <script>
+import BaseForm from "@/components/ui/BaseForm.vue";
+import BaseCard from "@/components/ui/BaseCard.vue";
+
 export default {
   name: "CoachRegistration",
+  components: { BaseCard, BaseForm },
+  data() {
+    return {
+      fields: [
+        { type: "text", id: "first-name", label: "Firstname" },
+        { type: "text", id: "last-name", label: "Lastname" },
+        { type: "textarea", id: "description", label: "Description" },
+        { type: "number", id: "hourly-rate", label: "Hourly rate" },
+        {
+          type: "options",
+          id: "areas",
+          label: "Areas of Expertise",
+          options: [
+            {
+              id: "frontend",
+              label: "Frontend Development",
+              value: "frontend",
+            },
+            {
+              id: "backend",
+              label: "Backend Development",
+              value: "backend",
+            },
+            {
+              id: "career",
+              label: "Career Advisory",
+              value: "career",
+            },
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
-  <h1>Coach Registration</h1>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <base-form :fields="fields" submit-button-text="Register!" />
+    </base-card>
+  </section>
 </template>
