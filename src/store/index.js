264ddc6af1c5deeac5bc6f5f8b5fb83979168bf8
stargazingsimple <stargazingsimple@gmail.com>
2026-02-16 16:49:53 +0200

    add action add coach

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
 import coaches from "@/store/modules/coaches";
 import requests from "@/store/modules/requests";
+import auth from "@/store/modules/auth";
 
 const store = createStore({
   modules: {
     coaches,
     requests,
+    auth,
   },
 });
 
