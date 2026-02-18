eaee75b8a06aa962a12ed8f337e64de2206f4589
stargazingsimple <stargazingsimple@gmail.com>
2026-02-18 18:43:18 +0200

    add form to coach contact component

--- src/pages/CoachContact.vue
+++ src/pages/CoachContact.vue
@@ -1,9 +1,44 @@
 <script>
+import validationSchema from "@/utils/validation/schemas/contact.js";
+import BaseForm from "@/components/ui/BaseForm.vue";
+
 export default {
   name: "CoachContact",
+  components: { BaseForm },
+  data() {
+    return {
+      fields: [
+        {
+          type: "email",
+          id: "email",
+          label: "Your Email",
+        },
+        {
+          type: "textarea",
+          id: "message",
+          label: "Message",
+        },
+      ],
+    };
+  },
+  computed: {
+    validationSchema() {
+      return validationSchema;
+    },
+  },
+  methods: {
+    contact(formData) {
+      console.log(formData);
+    },
+  },
 };
 </script>
 
 <template>
-  <h1>Coach Contact</h1>
+  <base-form
+    :validation-schema="validationSchema"
+    :fields="fields"
+    submit-button-text="Send Message"
+    @submit="contact"
+  />
 </template>
