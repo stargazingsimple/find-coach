976057205f8f6ba89d73ae5fd9a8e2b2d77b0b08
stargazingsimple <stargazingsimple@gmail.com>
2026-02-25 16:21:01 +0200

    add loader with overlay

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
+  <loading v-model:active="isLoading" color="#3d008d" />
   <the-header />
   <router-view />
 </template>
 
 <script>
+import "vue-loading-overlay/dist/css/index.css";
+import { mapGetters } from "vuex";
+import Loading from "vue-loading-overlay";
 import TheHeader from "@/components/layout/TheHeader.vue";
 
 export default {
   name: "App",
-  components: { TheHeader },
+  components: { TheHeader, Loading },
+  computed: {
+    ...mapGetters("loader", ["isLoading"]),
+  },
 };
 </script>
 
