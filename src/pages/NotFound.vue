196d2fd0ef322e1db67f473083644b1d6db021f1
stargazingsimple <stargazingsimple@gmail.com>
2026-02-27 23:11:58 +0200

    add content to not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,20 @@
 <script>
+import BaseCard from "@/components/ui/BaseCard.vue";
+
 export default {
   name: "NotFound",
+  components: { BaseCard },
 };
 </script>
 
 <template>
-  <h1>Not Found</h1>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
