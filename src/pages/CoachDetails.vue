2f4c92ad6694298f4c4fe50cf65fd0624ff6c711
stargazingsimple <stargazingsimple@gmail.com>
2026-02-27 23:00:26 +0200

    implement transition

--- src/pages/CoachDetails.vue
+++ src/pages/CoachDetails.vue
@@ -28,29 +28,33 @@ export default {
 </script>
 
 <template>
-  <section>
-    <base-card>
-      <h2>{{ selectedCoach.firstName }} {{ selectedCoach.lastName }}</h2>
-      <h3>${{ selectedCoach.hourlyRate }}/hours</h3>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>Interested? Reach out now!</h2>
-        <base-button link :path="`/coaches/${id}/contact`">Contact</base-button>
-      </header>
-      <router-view />
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <base-badge
-        v-for="area in selectedCoach.areas"
-        :key="area"
-        :type="area"
-      />
-      <p>{{ selectedCoach.description }}</p>
-    </base-card>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ selectedCoach.firstName }} {{ selectedCoach.lastName }}</h2>
+        <h3>${{ selectedCoach.hourlyRate }}/hours</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <base-button link :path="`/coaches/${id}/contact`"
+            >Contact</base-button
+          >
+        </header>
+        <router-view />
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in selectedCoach.areas"
+          :key="area"
+          :type="area"
+        />
+        <p>{{ selectedCoach.description }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
