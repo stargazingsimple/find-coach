00db8dc848190d6017a56dd5cd7d9383b8000ed4
stargazingsimple <stargazingsimple@gmail.com>
2026-03-05 12:10:23 +0200

    rework base checkbox filter component

--- src/components/ui/BaseCheckboxFilter.vue
+++ src/components/ui/BaseCheckboxFilter.vue
@@ -10,7 +10,7 @@ export default {
   emits: ["change-filter"],
   methods: {
     setFilterOption(event) {
-      const { id, checked } = event.target;
+      const { id, checked } = event;
       this.$emit("change-filter", { key: id, value: checked });
     },
   },
@@ -20,32 +20,17 @@ export default {
 <template>
   <v-card-base>
     <slot name="title" />
-    <span
-      v-for="{ id, label } in filterOptions"
-      :key="id"
-      class="filter-option"
-    >
-      <input type="checkbox" :id="id" checked @change="setFilterOption" />
-      <label :for="id">{{ label }}</label>
-    </span>
+    <div class="d-flex ga-2">
+      <v-checkbox
+        v-for="{ id, label, value } in filterOptions"
+        :key="id"
+        :label="label"
+        :model-value="value"
+        color="primary"
+        hide-details
+        density="compact"
+        @update:model-value="setFilterOption({ id, checked: $event })"
+      />
+    </div>
   </v-card-base>
 </template>
-
-<style scoped>
-.filter-option {
-  margin-right: 1rem;
-}
-
-.filter-option label,
-.filter-option input {
-  vertical-align: middle;
-}
-
-.filter-option label {
-  margin-left: 0.25rem;
-}
-
-.filter-option.active label {
-  font-weight: bold;
-}
-</style>
